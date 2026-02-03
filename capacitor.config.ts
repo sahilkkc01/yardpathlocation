ed148ff7d65cf3fbfe841859a8c16d084681c616
sahilkkc01 <sahilgupta5655@gmail.com>
2026-02-03 15:37:46 +0530

    5

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'pathyardlocation',
+  webDir: 'build'
+};
+
+export default config;
